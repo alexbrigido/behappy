9f2dde0711b49239024b0324296dce89a4fa2ce4
Alexandre Dias Brigido <f553996@corp.caixa.gov.br>
2020-03-05 14:58:55 -0300

    Added script to handle image scroller event

---
+++ src/models/Avatar.js
@@ -0,0 +1,11 @@
+class Avatar {
+    constructor(index,descricao) {
+        this.index = index;
+        this.descricao = descricao;
+    }
+
+    toString() {
+        return this.descricao;
+    }
+}
+export default Avatar;
